cb665ca54592338f2c32acd413cee0cac396b5d4
mreilly29 <mreilly18@jcu.edu>
2018-10-05 15:22:08 -0400

    initial commit

---
+++ HomeControllerTests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using ReviewSite.Controllers;
+using System;
+using Xunit;
+
+namespace ReviewSite.Tests
+{
+    public class HomeControllerTests
+    {
+            [Fact]
+            public void Index_Returns_A_View()
+            {
+                var underTest = new HomeController();
+
+                var result = underTest.Index();
+
+                Assert.IsType<ViewResult>(result);
+            }
+        
+    }
+}
