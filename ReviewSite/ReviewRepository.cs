b9f8cac040df9c63a15231847ca695d65482c27b
mreilly29 <mreilly18@jcu.edu>
2018-10-06 14:16:55 -0400

    added images to pages

--- ReviewSite/ReviewRepository.cs
+++ ReviewSite/ReviewRepository.cs
@@ -10,9 +10,9 @@ namespace ReviewSite
     {
         Dictionary<int, Review> allReviews = new Dictionary<int, Review>()
         {
-            { 1, new Review() {Id = 1, Content = "this is content 1" } },
-            { 2, new Review() {Id = 2, Content = "This is content for 2"} },
-            { 3, new Review() {Id = 3, Content = "This is content for 3"} },
+            { 1, new Review() {Id = 1, Title = "Corgi Type: Untoasted", ImageUrl = "/Images/whiteCorgi.png", ReviewCategory = "dog", Content = "Untoasted type Corgis have white fur." } },
+            { 2, new Review() {Id = 2, Title = "Corgi Type: Toasted", ImageUrl="/Images/puppyCorgi.jpg",ReviewCategory = "dog", Content = "Toasted type Corgis have brown fur."} },
+            { 3, new Review() {Id = 3, Title = "Corgi Type: Burnt", ImageUrl="/Images/blackCorgi.jpg", ReviewCategory="dog", Content = "Burnt type Corgis have mostly black fur with partial brown and white mixed in."} },
         };
     
         public Review FindById(int id)
